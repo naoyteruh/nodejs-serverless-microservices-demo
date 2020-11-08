3e5eff1180e2232319a99c5deba89c1c4e20ffc1
unknown <naoy.teruh@gmail.com>
2020-11-08 22:48:25 +0100

    gs-debug function profiling

--- gs-debug/index.js
+++ gs-debug/index.js
@@ -1,5 +1,14 @@
 const debug = require('debug.js');
 
+
+const traceApi = require('@google-cloud/trace-agent').start({
+    serviceContext: {
+        service: 'gs-debug'
+    }
+});
+const debug = require('@google-cloud/debug-agent').start();
+//   require('@google-cloud/profiler').start();
+
 /**
  * Generic background Cloud Function to be triggered by Cloud Storage.
  *
@@ -7,6 +16,12 @@ const debug = require('debug.js');
  * @param {object} context The event metadata.
  */
 exports.helloGCS = (file, context) => {
-    debug.debugContext(context);
+    
+    const contextSpan = traceApi.createChildSpan({name: 'context'});
+    debug.debugContext(context);    
+    contextSpan.endSpan();
+    const fileSpan = traceApi.createChildSpan({name: 'file'});
     debug.debugFile(file);
+    fileSpan.endSpan();
+
 };
